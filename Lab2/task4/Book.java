918cd9f9df2e481d49e2e5b513605c25a447a5d4
Alex Careja <44427360+alexcareja@users.noreply.github.com>
2019-11-04 10:29:34 +0200

    Add files via upload

---
+++ Lab2/task4/Book.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "\nTitle: " + this.title +
+                "\nAuthor: " + this.author +
+                "\nYear: " + Integer.toString(this.year);
+    }
+}
